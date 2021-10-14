4f06f4936b639055a00673ca197a973759ecc3cd
hsalucamba <hsalucamba@interdigitos.co.ao>
2021-10-14 11:53:40 +0100

    Variables declarations using access modifiers

---
+++ ContaBancaria.php
@@ -0,0 +1,16 @@
+<?php
+    class ContaBancaria {
+
+        private $banco;
+        private $nomeTitular;
+        private $numeroAgencia;
+        private $numeroContact;
+        private $saldo;
+
+
+    }
+
+
+    $conta = new ContaBancaria();
+
+    var_dump($conta->banco);
